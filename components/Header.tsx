f50a0ef17c1760e835019cd66746d3d3998c41fb
efp-coder <efpcode.project@gmail.com>
2025-12-02 16:00:59 +0100

    (adds) url helper function and dynamic section text for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,16 +2,22 @@
 
 import Button from "@/components/ui/Button"
 import NavBar from "@/components/NavBar";
+import prettifyPath from "@/helpers/prettifyPath";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
+
 export default function Header() {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => { setIsMenuOpen(!isMenuOpen); };
   const menuSymbol = isMenuOpen ? "x" : "≡";
+  const pathName = usePathname();
+  const sectionText = prettifyPath(pathName);
+
 
   return (
     <header className="flex bg-primary p-4 text-2xl justify-between items-center relative z-50">
-      <section>Hello</section>
+      <section className="bg-background text-accent p-2 rounded-md text-balance m-2">{sectionText}</section>
       <Button text={menuSymbol} onClick={toggleMenu} />
       {isMenuOpen && <NavBar closeMenu={toggleMenu} />}
     </header>
